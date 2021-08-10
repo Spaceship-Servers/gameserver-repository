9f2e8defae90df1172f865511d39fac8ad49195c
steph <sappho@sappho.io>
2021-08-10 07:41:44 -0400

    ${PWD}

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo `realpath ./`
-steamcmd/steamcmd.sh +login anonymous +force_install_dir `realpath ./` +app_update 232250 +exit
+
+# colors
+# source scripts/helpers.sh
+
+# a2s shennanigans
+export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
+export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
+
+steamcmd/steamcmd.sh +login anonymous +force_install_dir ${PWD} +app_update 232250 +exit
 ./srcds_run $*
