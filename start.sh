3b745cfbac5dab00840f7808800b2bd4cb4a8b7c
stephanie l <sappho@sappho.io>
2021-08-24 17:06:21 +0000

    print on start

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # colors
-# source scripts/helpers.sh
+source scripts/helpers.sh
 
 # a2s shennanigans
 export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
 export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
 
 steamcmd/steamcmd.sh +login anonymous +force_install_dir ${PWD} +app_update 232250 +exit
+
+ok "./srcds_run $*"
+
 ./srcds_run $*
