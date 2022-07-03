525e7c751a09505a6829a365acd19556e762f759
sapphonie <sappho@sappho.io>
2022-07-03 03:40:22 +0000

    bump mm and sm version (happy 1.11 stable, SM!)

--- scripts/getmmsm.sh
+++ scripts/getmmsm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mm_url="https://mms.alliedmods.net/mmsdrop/1.12/mmsource-1.12.0-git1157-linux.tar.gz"
-sm_url="https://www.sourcemod.net/smdrop/1.11/sourcemod-1.11.0-git6893-linux.tar.gz"
+mm_url="https://mms.alliedmods.net/mmsdrop/1.12/mmsource-1.12.0-git1160-linux.tar.gz"
+sm_url="https://www.sourcemod.net/smdrop/1.12/sourcemod-1.12.0-git6906-linux.tar.gz"
 
-mm_dest="mm-1.12.1157.tgz"
-sm_dest="sm-1.11.6893.tgz"
+mm_dest="mm-1.12.1160.tgz"
+sm_dest="sm-1.12.6906.tgz"
 
 export destfolder="mmsm_xtracted"
 
