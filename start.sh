378746f9ae20009f43642b6659f1a2da7816194e
sappho <sappho@sappho.io>
2023-03-18 10:07:35 -0400

    perf properly lol

--- start.sh
+++ start.sh
@@ -6,10 +6,12 @@ source scripts/helpers.sh
 export STEAM_GAMESERVER_RATE_LIMIT_200MS=25
 export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
 
-steamcmd/steamcmd.sh +force_install_dir ${PWD} +login anonymous +app_update 232250 +exit
+steamcmd/steamcmd.sh +force_install_dir "${PWD}" +login anonymous +app_update 232250 +exit
 
-ok "./srcds_run $*"
-#perf record
-./srcds_run $*
-# ; perf inject --jit --input perf.data --output perf.jit.data
-# ./srcds_run $*
+startdate=$(date +%Y_%m_%d__%H_%M_%S)
+mkdir ./perf || echo "perf dir already exists"
+find ./perf -mmin +1440 -exec rm -v {} \;
+perf record --output ./perf/"${startdate}"_perf.data \
+    ./srcds_run "$*"; \
+perf inject --jit --input ./perf/"${startdate}"_perf.data --output ./perf/"${startdate}"_perf.jit.data; \
+perf archive ./perf/"${startdate}"_perf.jit.data;
