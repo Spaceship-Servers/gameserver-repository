2990e65c8e147220d2efdbb78ef1a5a83d5c920f
sappho <sappho@sappho.io>
2023-03-18 08:21:19 -0400

    fuck perf in docker lol

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
 steamcmd/steamcmd.sh +force_install_dir ${PWD} +login anonymous +app_update 232250 +exit
 
 ok "./srcds_run $*"
-
-perf record ./srcds_run $* ; perf inject --jit --input perf.data --output perf.jit.data
+#perf record
+./srcds_run $*
+# ; perf inject --jit --input perf.data --output perf.jit.data
 # ./srcds_run $*
