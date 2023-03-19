3c55c26e27a5ac670b4ec8aebdcb00f6a00c10b7
sappho <sappho@sappho.io>
2023-03-19 06:01:22 -0400

    stop perfing, i got what i needed

--- start.sh
+++ start.sh
@@ -8,10 +8,12 @@ export STEAM_GAMESERVER_PACKET_HANDLER_NO_IPC=1
 
 steamcmd/steamcmd.sh +force_install_dir "${PWD}" +login anonymous +app_update 232250 +exit
 
-startdate=$(date +%Y_%m_%d__%H_%M_%S)
-mkdir ./perf || echo "perf dir already exists"
-find ./perf -mmin +1440 -exec rm -v {} \;
-perf record --output ./perf/"${startdate}"_perf.data \
-    ./srcds_run "$*"; \
-perf inject --jit --input ./perf/"${startdate}"_perf.data --output ./perf/"${startdate}"_perf.jit.data; \
-perf archive ./perf/"${startdate}"_perf.jit.data;
+./srcds_run "$*"
+
+#startdate=$(date +%Y_%m_%d__%H_%M_%S)
+#mkdir ./perf || echo "perf dir already exists"
+#find ./perf -mmin +1440 -exec rm -v {} \;
+#perf record --output ./perf/"${startdate}"_perf.data \
+#    ./srcds_run "$*"; \
+#perf inject --jit --input ./perf/"${startdate}"_perf.data --output ./perf/"${startdate}"_perf.jit.data; \
+#perf archive ./perf/"${startdate}"_perf.jit.data;
